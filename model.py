db86b3f10957a60ca32e53ec53ca594b59458503
Chi-Ning Chou <cchou@workergpu126.cm.cluster>
2023-11-21 21:42:35 -0500

    update model

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import os
 import time
+import json
 import torch
 import torch.nn as nn
 from torch.utils import data
@@ -12,14 +13,15 @@ print(device)
 
 
 IMAGE_SIZE = 32
-BATCH_SIZE = 256
-NUM_EPOCHS = 60
+BATCH_SIZE = 512
+NUM_EPOCHS = 150
 LEARNING_RATE = 0.001
 
 # CIFAR100_DIR = '/mnt/home/cchou/ceph/Data/'
-CIFAR100_DIR = '/mnt/home/cchou/ceph/Data/cifar100_train_processed'
+CIFAR100_TRAIN_DIR = '/mnt/home/cchou/ceph/Data/cifar100_train_processed'
+CIFAR100_TEST_DIR = '/mnt/home/cchou/ceph/Data/cifar100_test_processed'
 OUTPUT_DIR = '/mnt/home/cchou/ceph/Capstone/'
-CHECKPOINT_DIR = OUTPUT_DIR + '/CIFAR100_models'
+CHECKPOINT_DIR = OUTPUT_DIR + '/CIFAR100_models_epoch150'
 
 os.makedirs(CHECKPOINT_DIR, exist_ok=True)
 
@@ -38,7 +40,8 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = datasets.ImageFolder(CIFAR100_DIR, transform_train)
+trainset = datasets.ImageFolder(CIFAR100_TRAIN_DIR, transform_train)
+testset = datasets.ImageFolder(CIFAR100_TEST_DIR, transform_test)
 # trainset = datasets.CIFAR100(root=CIFAR100_DIR, train=True, download=True, transform=transform_train)
 # testset = datasets.CIFAR100(root=CIFAR100_DIR, train=False, download=True, transform=transform_train)
 
@@ -90,11 +93,11 @@ trainloader = data.DataLoader(
         drop_last=True,
         batch_size=BATCH_SIZE)
 
-# testloader = data.DataLoader(
-#         testset,
-#         num_workers=2,
-#         drop_last=True,
-#         batch_size=BATCH_SIZE)
+testloader = data.DataLoader(
+        testset,
+        num_workers=2,
+        drop_last=True,
+        batch_size=BATCH_SIZE)
 
 print('Dataloader created')
 
@@ -103,6 +106,10 @@ optimizer = torch.optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
 print('Optimizer created')
 
 criterion = nn.CrossEntropyLoss()
+
+# Lists to store training and test errors
+train_errors = []
+test_errors = []
 
 
 print('Starting training...')
@@ -122,24 +129,58 @@ for epoch in range(NUM_EPOCHS):
         optimizer.step()
 
         total_loss += loss.item()
-    end_time = time.time()
+   
 
     # save checkpoints
     checkpoint_path = os.path.join(CHECKPOINT_DIR, 'model_states_e{}.pkl'.format(epoch + 1))
     torch.save(model.state_dict(), checkpoint_path)
-
+    
+    # Calculate train error
     model.eval()
-    correct = 0
-    total = 0
+    correct_train = 0
+    total_train = 0
     with torch.no_grad():
         for data in trainloader:
             images, labels = data
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
             _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            total_train += labels.size(0)
+            correct_train += (predicted == labels).sum().item()
 
-    val_acc = 100 * correct / total
-    print(f"Epoch: {epoch + 1}, Train Accuracy: {val_acc:.2f}%", "Average train Loss:", total_loss/len(trainloader), "Time per epoch", end_time-start_time)
+    train_acc = 100 * correct_train / total_train
+    train_errors.append(100 - train_acc)
+    
+    # Calculate test error
+    model.eval()
+    correct_test = 0
+    total_test = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total_test += labels.size(0)
+            correct_test += (predicted == labels).sum().item()
 
+    test_acc = 100 * correct_test / total_test
+    test_errors.append(100 - test_acc)
+    
+    end_time = time.time()
+    
+    results = {
+        'train_errors': train_errors,
+        'test_errors': test_errors
+    }
+    
+    with open(OUTPUT_DIR+'train_test_errors.json', 'w') as json_file:
+        json.dump[(results, json_file)
+    print("Results saved in json")
+    
+        
+    print(f"Epoch: {epoch + 1}, Train Accuracy: {train_acc:.2f}%, Test Accuracy: {test_acc:.2f}%", "Average train Loss:", total_loss/len(trainloader), "Time per epoch", end_time-start_time)
+
+# Print the final errors
+print("Train Errors:", train_errors)
+print("Test Errors:", test_errors)
